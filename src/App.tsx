9490a0ce79e4e79193120e39450e28dcfa800223
Luis Arrieta <luisarrieta796@gmail.com>
2025-04-10 16:24:33 -0500

    build: integrate Tailwind CSS and configure Vite for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export default function App () {
   return (
-    <div>App</div>
+    <div className="w-full min-h-screen bg-sky-700 text-sky-300 font-bold flex justify-center items-center text-5xl">
+      Tailwind Works!
+    </div>
   )
 }
